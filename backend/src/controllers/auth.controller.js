6e2925c31b602d0b250601d8725afa2b899ed9ed
gajendra950 <yaduvanshigajendra2002@gmail.com>
2025-03-08 14:34:36 +0530

    Auth controllers

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,96 @@
-export const signup =  (req,res) => {
-    res.send("signup route");
-};
+import User from "../models/user.model.js";  
+import bcrypt from "bcryptjs";           
+import  {generateToken} from "../lib/utils.js"; 
+//signup
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        
+       if(!fullName || ! email || !password) {
+        return res.status(400).json({ message: "All fields are Required" });
+       }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
 
-export const login = (req,res) => {
-    res.send("login route");
-};
+        const existingUser= await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
 
-export const logout = (req,res) => {
-    res.send("logout route");
-};
+        const salt = await bcrypt.genSalt(10);   
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if(newUser) {
+           
+             generateToken (newUser._id, res);
+             await newUser.save();
+             res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+             });
+        }
+          else {
+             res.status(400).json({
+                 message : "Invalid user data"
+             });
+          }
+        }
+            catch (error) {
+                 console.log("Error in signup controller",error.message);
+                 res.status(500).json( { message : "Internal Server Error"});
+            }
+        };
+          //login
+
+            export const login = async (req, res) => {
+                const { email, password } = req.body;
+            
+                try {
+                
+                    const user = await User.findOne({ email });
+                    if (!user) {
+                        console.log("User not found for email:", email);
+                        return res.status(400).json({ message: "Invalid credentials" });
+                    }
+            
+                    const isPasswordCorrect= await bcrypt.compare(password, user.password);
+                    if (!isPasswordCorrect) {
+                        console.log("Incorrect password for email:", email);
+                        return res.status(400).json({ message: "Invalid credentials" });
+                    }
+            
+                    generateToken(user._id, res);
+                    res.status(200).json({
+                        _id: user._id,
+                        fullName: user.fullName,
+                        email: user.email,
+                        profilePic: user.profilePic,
+                    });
+            
+                } catch (error) {
+                    console.error("Error in login controller:", error.message);
+                    res.status(500).json({ message: "Internal Server Error" });
+                }
+        };
+          //logout
+        export const logout = (req,res) => {
+              try {
+                 res.cookie("jwt","",{maxage:0})
+                 res.status(200).json( { message:"Logged out successfully"});
+              }
+               catch (error){
+                   console.log("Error in Logout Controller",error.message);
+                   res.status(500).json({ message: "Internal Server Error" });
+               }
+        };
+    
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,9 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT;
 
+const PORT = process.env.PORT;
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
